96642405c5a7facbe6c7b94c97a4e70b1a0dfcd9
Irsyad Purbha <irsyadpurbha@gmail.com>
2025-05-01 08:53:43 +0700

    FEAT<LATIHAN5>: ADD LATIHAN 5

--- Latihan5/app/src/main/java/com/example/latihan5/MainActivity2.kt
+++ Latihan5/app/src/main/java/com/example/latihan5/MainActivity2.kt
@@ -1,42 +1,47 @@
 package com.example.latihan5
 
-import android.content.Intent
 import android.os.Bundle
+import android.os.Debug
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.Console
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
 
-    private lateinit var buttonGO: Button
+    private lateinit var buttonKembali: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         Initiate_Object()
-        Listen_ButtonGO()
+        Listen_ButtonBack()
         val toast = Toast.makeText(this, "onCreate dipanggil", Toast.LENGTH_SHORT) // in Activity
         toast.show()
     }
 
     fun Initiate_Object() {
-        buttonGO = findViewById(R.id.button_Main_Go)
+        buttonKembali = findViewById(R.id.button_Main2_Back)
     }
 
-    fun Listen_ButtonGO() {
-        buttonGO.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
+    fun Listen_ButtonBack() {
+        buttonKembali.setOnClickListener {
+            this.finish()
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        val toast = Toast.makeText(this, "onDestroy dipanggil", Toast.LENGTH_SHORT) // in Activity
+        toast.show()
+    }
 }
